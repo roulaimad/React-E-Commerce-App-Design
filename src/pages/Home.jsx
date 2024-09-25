afd44d66fcd15e13fe3e9c4ed6e85c4bb2403afa
imad <roulaimad7@gmail.com>
2024-09-25 12:49:50 +0100

    Adding Navbar and Announcements

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Announcement from "../components/Announcement";
+
+function Home() {
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;
