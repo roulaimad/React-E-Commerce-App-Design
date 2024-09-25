f04745f45932f87f03921217143ed2a4fd6e1cf5
imad <roulaimad7@gmail.com>
2024-09-25 22:02:30 +0100

    adding Categories and CategoriesItem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import Announcement from "../components/Announcement";
 import Slider from "../components/Slider";
+import Categories from "../components/Categories";
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Announcement />
       <Navbar />
       <Slider />
+      <Categories />
     </div>
   );
 }
