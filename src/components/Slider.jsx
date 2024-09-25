3dfa91f63594852cab1d8c3a5c61a7e1ec068f5e
imad <roulaimad7@gmail.com>
2024-09-25 22:36:51 +0100

    adding Products and Product components

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -86,7 +86,7 @@ function Slider() {
 
       <Wrapper slideIndex={slideIndex}>
         {sliderItems.map((item) => (
-          <Slide bg={item.bg}>
+          <Slide bg={item.bg} key={item.id}>
             <ImgContainer>
               <Image src={item.img} />
             </ImgContainer>
