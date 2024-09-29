a1d1acddae0f4df1d443aefe27f302e9596e27c0
imad <roulaimad7@gmail.com>
2024-09-29 19:16:47 +0100

    adding products list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./Navbar";
+import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement";
 import Slider from "../components/Slider";
 import Categories from "../components/Categories";
